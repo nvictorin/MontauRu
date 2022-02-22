9862cc4bd57bef416f2c58fa43c1b5152a3df711
Nathan Victorin <nvictorin@iutbayonne.univ-pau.fr>
2022-02-23 00:57:58 +0100

    Algorithme des méthodes seConnecter() et sinscrire() dans Utilisateur.java

--- app/src/main/java/com/example/testbd/Utilisateur.java
+++ app/src/main/java/com/example/testbd/Utilisateur.java
@@ -37,6 +37,35 @@ public class Utilisateur
     public int getEstAdmin() { return estAdmin; }
     public void setEstAdmin(int a) { this.estAdmin = a; }
 
+    public void seConnecter()
+    {
+        //-------------------------------VARIABLES------------------------------------
+        //Récupération des valeurs des champs de saisie de l'email et mdp
+        //mail = ...;
+        //password = ...;
+        //-------------------------------TRAITEMENT------------------------------------
+        //Si (mail == email de l'utilisateur) && (password == mdp de l'utilisateur)
+            //Alors affiche "Connexion établie" sur un pop up ephémere (j'ai oublié le nom)
+                //Si estAdmin de l'utilisateur == 0
+                    //Renvoie sur la page d'accueil à l'état connecté non admin (client)
+                //Sinon renvoie sur la page d'accueil à l'état connecté admin (employé CROUS)
+        //Sinon affiche "Email ou mot de passe erronné"
+    }
+
+    public void sinscrire()
+    {
+        //-------------------------------VARIABLES------------------------------------
+        //Récupération des valeurs des champs de saisie de l'email et mdp
+        //mail = ...;
+        //password = ...;
+        //nom = ...;
+        //prenom = ...;
+        //-------------------------------TRAITEMENT------------------------------------
+        //Si mail déjà utilisé pour un compte
+            //Alors affiche "Cette adresse email est déjà utilisée"
+        //Sinon Utilisateur(mail, password, nom, prenom); (constructeur avec estAdmin à 0 par défaut)
+    }
+
     @Override
     public String toString()
     {
